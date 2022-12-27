349cf9c74263feefca71bfaf2efb86cde6e1e9ec
pranavv <pranavvyas2003@gmail.com>
2022-12-28 00:51:21 +0530

    feat: Send location through intent

--- app/src/main/java/com/example/daggerhilttest/activities/LocationSplashActivity.kt
+++ app/src/main/java/com/example/daggerhilttest/activities/LocationSplashActivity.kt
@@ -1,47 +1,64 @@
 package com.example.daggerhilttest.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.edit
+import androidx.lifecycle.viewModelScope
 import com.birjuvachhani.locus.Locus
+import com.example.daggerhilttest.PreferencesKeys
+import com.example.daggerhilttest.R
+import com.example.daggerhilttest.models.LatLong
 import com.example.daggerhilttest.viewmodels.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
+@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class LocationSplashActivity : AppCompatActivity() {
     private val TAG = "activity_lifecycle"
     private val weatherViewModel: WeatherViewModel by viewModels()
+    private lateinit var savedLatLong: LatLong
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        weatherViewModel
-        CoroutineScope(Dispatchers.IO).launch {
-            weatherViewModel.getLatLongFromDataStorePref().let {
-                if (it != null) {
-                    startWeatherActivity()
+        setContentView(R.layout.activity_location_splash)
+        onStartUp()
+    }
+
+    private fun onStartUp() {
+        CoroutineScope(Dispatchers.Main).launch {
+            weatherViewModel.getLatLongFromDataStorePref().let {Latlong ->
+                if (Latlong != null) {
+                    startWeatherActivity(Latlong)
                 } else {
                     getLocation()
                 }
             }
         }
     }
-
     private fun getLocation() {
         Locus.getCurrentLocation(this) { result ->
             result.location?.let {
                 weatherViewModel.saveLatLongInDataStorePref(it.latitude, it.longitude)
-                startWeatherActivity()
+                savedLatLong = LatLong(it.latitude, it.longitude)
+                startWeatherActivity(savedLatLong)
             }
+            Toast.makeText(this, "Location Fetched", Toast.LENGTH_SHORT).show()
             result.error?.let {  }
         }
     }
 
-    private fun startWeatherActivity() {
+    private fun startWeatherActivity(savedLatLong: LatLong) {
         val intent = Intent(this@LocationSplashActivity, MainActivity::class.java)
+        intent.putExtra("saved_lat", savedLatLong.lat)
+        intent.putExtra("saved_long", savedLatLong.long)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
         finish()
--- app/src/main/java/com/example/daggerhilttest/activities/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilttest/activities/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.daggerhilttest.models.LatLong
 import com.example.daggerhilttest.screens.CurrentWeatherScreen
 import com.example.daggerhilttest.ui.theme.DaggerHiltTestTheme
 import com.example.daggerhilttest.viewmodels.WeatherViewModel
@@ -20,14 +21,17 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val savedLat = intent.getDoubleExtra("saved_lat", 0.0)
+        val savedLong = intent.getDoubleExtra("saved_long", 0.0)
         setContent {
             DaggerHiltTestTheme {
-                val weatherViewModel = hiltViewModel<WeatherViewModel>()
+                val weatherViewModel: WeatherViewModel by viewModels()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = Color(0xFFF5F6F8)
                 ) {
-                    CurrentWeatherScreen(weatherViewModel = weatherViewModel)
+                    val savedLatLog = LatLong(savedLat, savedLong)
+                    CurrentWeatherScreen(weatherViewModel = weatherViewModel, savedLatLog)
                 }
             }
         }
