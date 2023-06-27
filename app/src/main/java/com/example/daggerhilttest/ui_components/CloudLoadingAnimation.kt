20c086da10335ccdfc299b40a2a3b27121476d9f
pranav <pranavvyas2003@gmail.com>
2023-06-27 23:36:43 +0530

    create repeating clouds animation

---
+++ app/src/main/java/com/example/daggerhilttest/ui_components/CloudLoadingAnimation.kt
@@ -0,0 +1,91 @@
+package com.example.daggerhilttest.ui_components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.animateValue
+import androidx.compose.animation.core.animateValueAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.example.daggerhilttest.R
+
+@Preview
+@Composable
+fun RepeatingCloudsAnimation() {
+    var animationRunning by remember { mutableStateOf(false) }
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val slideOffset by infiniteTransition.animateFloat(
+        initialValue = -2000f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            repeatMode = RepeatMode.Restart,
+            animation = tween(durationMillis = 1500)
+        )
+    )
+    val fadeAlpha by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 500, 1000),
+            repeatMode = RepeatMode.Restart
+
+        ),
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        if (!animationRunning) {
+            Button(
+                onClick = { animationRunning = true },
+                modifier = Modifier.align(Alignment.BottomEnd)
+            ) {
+                Text("Start Animation")
+            }
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.loading_clouds),
+                contentDescription = "Image",
+                modifier = Modifier
+                    .offset { IntOffset(slideOffset.toInt(), 0) }
+                    .alpha(fadeAlpha)
+                    .fillMaxSize()
+            )
+        }
+    }
+}
