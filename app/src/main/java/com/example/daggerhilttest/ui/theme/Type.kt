70bab9d8458322b120279ca314d2b4d411d91072
pranav <pranavvyas2003@gmail.com>
2023-07-13 01:27:11 +0530

    add product sans font

--- app/src/main/java/com/example/daggerhilttest/ui/theme/Type.kt
+++ app/src/main/java/com/example/daggerhilttest/ui/theme/Type.kt
@@ -2,10 +2,16 @@ package com.example.daggerhilttest.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.daggerhilttest.R
 
+// Custom fonts
+val productSans = FontFamily(
+    Font(R.font.product_sans_regular)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
