581994b4a0a7fc4a21abe3ef0a9e7242d26a584d
pranav <pranavvyas2003@gmail.com>
2023-07-30 03:21:28 +0530

    create base class for api calls

---
+++ app/src/main/java/com/example/daggerhilttest/models/BaseApiResponse.kt
@@ -0,0 +1,22 @@
+package com.example.daggerhilttest.models
+import android.util.Log
+import com.example.daggerhilttest.util.Resource_v2
+import retrofit2.Response
+abstract class BaseApiResponse {
+    suspend fun <T> safeApiCall(apiCall: suspend () -> Response<T>): Resource_v2<T> {
+        try {
+            val response = apiCall()
+            Log.d("weather_api_call_v2",response.body().toString())
+            if (response.isSuccessful) {
+                val body = response.body()
+                body?.let {
+                    return Resource_v2.Success(body)
+                }
+            }
+            return Resource_v2.Error(response.message().toString())
+        } catch (e: Exception) {
+            Log.d("weather_api_call_v2",e.toString())
+            return Resource_v2.Error(e.message)
+        }
+    }
+}
