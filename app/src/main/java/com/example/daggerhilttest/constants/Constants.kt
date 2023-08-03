59468e153674546807c532681f64c46fbc371067
pranav <pranavvyas2003@gmail.com>
2023-08-04 01:54:39 +0530

    add url's for new weather icons

--- app/src/main/java/com/example/daggerhilttest/constants/Constants.kt
+++ app/src/main/java/com/example/daggerhilttest/constants/Constants.kt
@@ -2,8 +2,12 @@ package com.example.daggerhilttest.constants
 
 object Constants {
     const val WEATHER_API_KEY = "32d0d01a465884c72557ddb727101c4f"
+    const val WEATHER_API_KEY_v2= "MXUPKH5T9B3ELZEEFR3BF9WPR"
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    const val BASE_URL_v2 = "https://weather.visualcrossing.com/"
     const val BASE_ICON_URL = "https://openweathermap.org/img/wn/"
+    const val BASE_WEATHER_ICON_URL = "https://raw.githubusercontent.com/PranavVyas20/Weather-Icons/ce918eb7cdcd1a3a3ba27e7c0677cb598ccab3bc/icons/"
+    const val WEATHER_ICON_URL_SUFFIX = ".svg"
     const val  ICON_URL_SUFFIX = "@4x.png"
     const val USER_PREFERENCES_NAME = "user_preferences"
 
