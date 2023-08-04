e1a348f649cd0fca717176994f68cac8abe4c4f5
pranav <pranavvyas2003@gmail.com>
2023-08-05 01:09:55 +0530

    create weather forecast data class

---
+++ app/src/main/java/com/example/daggerhilttest/models/WeatherForecastData.kt
@@ -0,0 +1,9 @@
+package com.example.daggerhilttest.models
+
+data class WeatherForecastData(
+    val temp: Double,
+    val minTemp: Double,
+    val maxTemp: Double,
+    val icon: String,
+    val dateTime: String
+)
